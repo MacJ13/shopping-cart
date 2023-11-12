d5360c1b0f006ff1795bcb6904028602202daffd
MacJ13 <jacdim02@gmail.com>
2023-11-12 21:48:50 +0100

    refactor: Add and style basic product elements

--- src/pages/product/index.jsx
+++ src/pages/product/index.jsx
@@ -1,6 +1,9 @@
 import { useParams } from "react-router-dom";
 import { useFetch } from "../../hooks/useFetch";
 import { imgRegex } from "../../utils/constants";
+import style from "./Product.module.scss";
+
+import Rating from "../../components/rating/Rating";
 
 const Product = () => {
   const { productId } = useParams();
@@ -11,15 +14,37 @@ const Product = () => {
 
   const [data, error, loading] = useFetch(url);
 
-  console.log(data);
-
   const images = data.images?.reduce((acc, current) => {
     if (current.primary || imgRegex.test(current.rel))
       return [...acc, { ...current, number: acc.length }];
     return acc;
   }, []);
 
-  return <div>{data.name}</div>;
+  return (
+    <div className={style.product}>
+      <div className={style.row}>
+        <div className={style.col}>
+          <div className={style.heading}>
+            <h1>{data.name}</h1>
+            <div className={style.content}>
+              <div className={style.catalog}>
+                <strong>Catalog number: </strong> {data.sku}
+              </div>
+              <Rating
+                reviewAvarage={data.customerReviewAverage}
+                reviewCount={data.customerReviewCount}
+                large={true}
+              />
+            </div>
+            <div className={style.price}>${data.regularPrice}</div>
+          </div>
+          <p className={style.p}>{data.longDescription}</p>
+          <button className={style.button}>Add to cart</button>
+        </div>
+        <div className={style.col}>{/* place for gallery image */}</div>
+      </div>
+    </div>
+  );
 };
 
 export default Product;
