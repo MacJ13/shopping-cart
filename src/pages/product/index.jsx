017a73f8940a96a1d9f2fd0978682f893775a901
MacJ13 <jacdim02@gmail.com>
2023-11-12 21:40:12 +0100

    refactor: fetch product data by productId url parameter

--- src/pages/product/index.jsx
+++ src/pages/product/index.jsx
@@ -1,5 +1,25 @@
+import { useParams } from "react-router-dom";
+import { useFetch } from "../../hooks/useFetch";
+import { imgRegex } from "../../utils/constants";
+
 const Product = () => {
-  return <div>Product</div>;
+  const { productId } = useParams();
+
+  const url = `https://api.bestbuy.com/v1/products/${productId}.json?show=sku,name,longDescription,manufacturer,features.feature,details.value,details.name,quantityLimit,image,regularPrice,includedItemList.includedItem,images,customerReviewCount,customerReviewAverage,thumbnailImage,modelNumber&apiKey=${
+    import.meta.env.VITE_API_KEY
+  }`;
+
+  const [data, error, loading] = useFetch(url);
+
+  console.log(data);
+
+  const images = data.images?.reduce((acc, current) => {
+    if (current.primary || imgRegex.test(current.rel))
+      return [...acc, { ...current, number: acc.length }];
+    return acc;
+  }, []);
+
+  return <div>{data.name}</div>;
 };
 
 export default Product;
