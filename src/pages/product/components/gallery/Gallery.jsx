c32f27275f4b1b770e98d1b9cd86593fa7c2b643
MacJ13 <jacdim02@gmail.com>
2023-11-13 13:32:11 +0100

    refactor: add and style slider in Gallery component

--- src/pages/product/components/gallery/Gallery.jsx
+++ src/pages/product/components/gallery/Gallery.jsx
@@ -4,22 +4,50 @@ import style from "./Gallery.module.scss";
 
 const Gallery = ({ images }) => {
   const [currentImage, setCurrentImage] = useState(images[0]);
-  //   const [range, setRange] = useState(0);
+  const [range, setRange] = useState(0);
 
-  //   const interval = 5;
-  //   const imageLength = images.length;
-  //   const start = range;
-  //   const end = range + interval;
+  const interval = 5;
+  const imageLength = images.length;
+  const start = range;
+  const end = range + interval;
 
-  //   const galleryImages = images.slice(start, end);
+  const galleryImages = imageLength <= 5 ? images : images.slice(start, end);
 
   return (
     <div className={style.gallery}>
       <img
+        data-testid="primary"
         className={style.primary}
         src={currentImage.href}
         alt={currentImage.rel}
       />
+      {imageLength > 1 && (
+        <div className={style.slider} data-testid="slider">
+          {galleryImages.map((image) => (
+            <button
+              key={image.rel}
+              className={`${style.thumbnail} ${
+                image.href === currentImage.href ? style.active : ""
+              }`}
+              onClick={() => {
+                setCurrentImage(image);
+
+                if (image.number < 2) setRange(0);
+                else if (
+                  image.number >= 2 &&
+                  image.number <= imageLength - 1 - 2
+                )
+                  setRange(image.number - 2);
+                else {
+                  setRange(imageLength - interval);
+                }
+              }}
+            >
+              <img src={image.href} alt={image.rel + "_slider"} />
+            </button>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
