1fc7df67f74075dd57251e567adf909102a42db8
MacJ13 <jacdim02@gmail.com>
2023-11-14 17:05:53 +0100

    refactor: add and style curve background in specifications

--- src/pages/product/components/specifications/Specifications.jsx
+++ src/pages/product/components/specifications/Specifications.jsx
@@ -45,6 +45,19 @@ const Specifications = ({ features, accessories, details }) => {
 
   return (
     <div className={style.spec}>
+      <div className={style.curveTop}>
+        <svg
+          data-name="Layer 1"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 1200 120"
+          preserveAspectRatio="none"
+        >
+          <path
+            d="M600,112.77C268.63,112.77,0,65.52,0,7.23V120H1200V7.23C1200,65.52,931.37,112.77,600,112.77Z"
+            className={style.shapeFill}
+          ></path>
+        </svg>
+      </div>
       <div className="container">
         <div className={style.controls}>
           {SPECS.map((spec) => {
