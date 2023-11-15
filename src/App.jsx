4f49341c397f313e352f2b57b7067158bbe69b0d
MacJ13 <jacdim02@gmail.com>
2023-11-15 17:48:34 +0100

    refactor: Add Cart to App component

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { Outlet } from "react-router-dom";
 import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
 import ScrollToTop from "./components/scrollToTop/ScrollToTop";
+import Cart from "./components/cart/Cart";
 
 function App() {
   return (
     <>
       <ScrollToTop />
       <Navbar />
+      <Cart />
       <Outlet />
       <Footer />
     </>
