97231bbcb602f15258c5538e90be86ee0791d000
MacJ13 <jacdim02@gmail.com>
2023-11-15 12:08:37 +0100

    feat: add ScrollToTop inside App

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
+import ScrollToTop from "./components/scrollToTop/ScrollToTop";
 
 function App() {
   return (
     <>
+      <ScrollToTop />
       <Navbar />
       <Outlet />
       <Footer />
