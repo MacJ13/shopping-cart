3df16daa4b278a339be1d3b62750ad7be56a2d01
MacJ13 <jacdim02@gmail.com>
2023-11-15 17:44:13 +0100

    refactor: Wrap router with StoreProvider component

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,10 +1,15 @@
 import { RouterProvider, createHashRouter } from "react-router-dom";
 import { routerConfig } from "./routerConfig";
+import StoreProvider from "../context/StoreContext";
 
 const Router = () => {
   const routes = createHashRouter(routerConfig);
 
-  return <RouterProvider router={routes} />;
+  return (
+    <StoreProvider>
+      <RouterProvider router={routes} />;
+    </StoreProvider>
+  );
 };
 
 export default Router;
